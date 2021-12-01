359f200e1515dbd06a3d8b1476bbe4d651f099ff
Viviane Puga <vnpuga@gmail.com>
2021-12-01 16:00:44 -0300

    exercício de fixar for..in

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/fixarForIn.js
@@ -0,0 +1,24 @@
+// 1 - Usando o objeto abaixo, utilize For/in e imprima no console a mensagem 'Olá xxxxx' para cada nome, substituindo o xxxxx pelo nome em questão.
+
+let names = {
+  person1: 'João',
+  person2: 'Maria',
+  person3: 'Jorge' 
+  };
+
+  for (let key in names) {
+    console.log('Olá ' + names[key] + ', seja bem vindo!');
+  }
+
+
+// 2 - Usando o objeto abaixo, utilize For/in e imprima um console.log com as chaves e valores desse objeto.
+
+let car = {
+  model: 'A3 Sedan',
+  manufacturer: 'Audi',
+  year: 2020
+};
+
+for (const key in car) {
+  console.log(key, car[key]);    
+}

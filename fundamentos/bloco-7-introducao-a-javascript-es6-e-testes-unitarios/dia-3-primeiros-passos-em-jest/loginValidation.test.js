f44190a85629d3b2143c523a80f6bb2f8a182871
Viviane Puga <vnpuga@gmail.com>
2022-01-18 13:04:44 -0300

    exercicios do dia

---
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/loginValidation.test.js
@@ -0,0 +1,39 @@
+// exemplo do conteúdo "escrevendo testes"
+
+// loginValidation.test.js
+const {
+  greetingMessage,
+  loginErrorMessage,
+  verifyCredentials,
+} = require("./loginValidation.js");
+
+describe("a função verifyCredentials()", () => {
+  
+  it("verifyCredentials() calls the correct function depending on the user and password input", () => {
+    
+    const user = {
+      userName: 'Bob',
+      password: 123456,
+    };
+      
+    const { userName, password } = user;
+
+    expect(verifyCredentials({ userName, password })).toBe(
+      "Hello, Joana! Que bom ter você de volta"
+    ); 
+  });
+
+  it("greetingMessage() returns a message in the format: `Hello, ${user}! Que bom ter você de volta`", () => {
+    expect(greetingMessage("Lucas")).toBe(
+      "Hello, Lucas! Que bom ter você de volta"
+    );
+  });
+
+  it("loginErrorMessage() returns a message in the format: `Pessoa usuária '${user}' não encontrada, tente novamente!`", () => {
+    expect(loginErrorMessage("Bob")).toBe(
+      "Pessoa usuária 'Bob' não encontrada, tente novamente!"
+    );
+  });  
+});
+
+//Outro ponto positivo da utilização do Jest para fazer nossos testes é que ele traz uma mensagem contendo um diff, ou seja, o que era esperado de ocorrer no teste e o que de fato aconteceu. Isso nos ajuda a entender mais rapidamente onde está o erro. Neste exemplo o teste esperava receber como valor um objeto contendo uma pessoa usuária com o nome "Joana", mas ao invés disso recebeu "Bob".

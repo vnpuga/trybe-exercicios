ecc61565fc39249d7a84b2f8446cdf841730b309
Viviane Puga <vnpuga@gmail.com>
2022-02-17 15:06:59 -0300

    exercicio de fixacao de conteudo

---
+++ front-end/bloco-10-introducao-a-react/dia-2-componentes-react/fixation-exercises-10-2/src/Image.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Image extends React.Component {
+  render() {
+    return <img src={this.props.source} alt={this.props.alternativeText} />;
+  }
+}
+
+export default Image;
+
+// 1.Qual o nome do componente declarado acima? Resposta: Image
